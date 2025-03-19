33c7e9b902d2fe547ae441e35a5085727c3307cb
sneha siripuram <snehasweety708@gmail.com>
2025-03-19 14:51:35 -0600

    commit

--- e2e/Pages/PharmacyPage.js
+++ e2e/Pages/PharmacyPage.js
@@ -1,33 +1,35 @@
 
 class PharmacyPage{
-    zipcodebtn='#mat-radio-14 > .mat-radio-label > .mat-radio-container > .mat-radio-outer-circle'
+    zipcodebtn='#mat-radio-14 > .mat-radio-label > .mat-radio-container > .mat-radio-inner-circle'
     addressbtn='#mat-radio-15 > .mat-radio-label > .mat-radio-container > .mat-radio-outer-circle'
     zipele='#mat-input-13'
     Ziphistoryarrow='.mat-select-arrow'
-    addressEle='#mat-input-14'
+    //addressEle='#mat-input-14'
     zipfindphramacyelement=':nth-child(4) > .mat-focus-indicator > .mat-button-wrapper'
     AddressfindpharmacyELe='.form-field-container-col2 > :nth-child(3) > .mat-focus-indicator'
     distanceele='#mat-input-12'
     AddressphramcynameEle='#mat-input-11'
-    selecttheziphistoryele='#mat-option-21 > .mat-option-text'
+    selecttheziphistoryele='#mat-option-20 > .mat-option-text'
     nextpageBtnEle=':nth-child(2) > .paginator-wrapper > .mat-paginator > .mat-paginator-outer-container > .mat-paginator-container > .mat-paginator-range-actions > .mat-paginator-navigation-next'
     lastpageBtnEle=':nth-child(2) > .paginator-wrapper > .mat-paginator > .mat-paginator-outer-container > .mat-paginator-container > .mat-paginator-range-actions > .mat-paginator-navigation-last'
     periviouspageBtnEle='.mat-paginator-navigation-previous'
     firstpageBtnEle='body > app-root:nth-child(1) > div:nth-child(1) > div:nth-child(2) > mat-sidenav-container:nth-child(1) > mat-sidenav-content:nth-child(5) > app-pharmacy:nth-child(3) > div:nth-child(3) > app-pharmacy-list:nth-child(1) > div:nth-child(2) > div:nth-child(2) > mat-paginator:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > button:nth-child(2) > span:nth-child(1) > svg:nth-child(1)'
-    pharmacycheckBoxEle='#mat-checkbox-106 > .mat-checkbox-layout > .mat-checkbox-inner-container'
-    secondpharmacyCheckEle='#mat-checkbox-107 > .mat-checkbox-layout > .mat-checkbox-inner-container'
+    pharmacycheckBoxEle='#mat-checkbox-26 > .mat-checkbox-layout > .mat-checkbox-inner-container'
+    secondpharmacyCheckEle='#mat-checkbox-27 > .mat-checkbox-layout > .mat-checkbox-inner-container'
+    thirdpharmacyCheckEle='#mat-checkbox-28 > .mat-checkbox-layout > .mat-checkbox-inner-container'
+    fourthpharmacyCheckele='#mat-checkbox-29 > .mat-checkbox-layout > .mat-checkbox-inner-container'
+    fifthpharmacycheckEle='#mat-checkbox-31 > .mat-checkbox-layout > .mat-checkbox-inner-container'
     googlemapRadioBtnEle='app-pharmacy-card:nth-child(2) div:nth-child(1) mat-card:nth-child(1) mat-card-header:nth-child(1) div:nth-child(1) mat-card-title:nth-child(1) div:nth-child(1) div:nth-child(3) span:nth-child(1) mat-icon:nth-child(1)'
     itemperPageEle=':nth-child(2) > .paginator-wrapper > .mat-paginator > .mat-paginator-outer-container > .mat-paginator-container > .mat-paginator-page-size'
     nextbtnEle='.selected-pharmacy > .button-wrapper'
     backbtnEle='.button-wrapper > .mat-focus-indicator'
-    //pharmacycheckEle='#mat-checkbox-93 > .mat-checkbox-layout > .mat-checkbox-inner-container'
-    zipcoderadiocheckele='input[type=radio][value=1]'
-    pickpharmacyfromadddressEle='#mat-checkbox-121 > .mat-checkbox-layout > .mat-checkbox-inner-container'
-    choosepharmacyaddresele='#mat-checkbox-130 > .mat-checkbox-layout > .mat-checkbox-inner-container'
-    checkpharmacyagainfromaddressele='#mat-checkbox-154 > .mat-checkbox-layout > .mat-checkbox-inner-container'
+
 
     clickZipcoderadio(){
         cy.get(this.zipcodebtn).click({force:true})
+    }
+    verifyZipcodeSelected() {
+        cy.get(this.zipcodebtn).should('be.checked');
     }
     enterZipcode(zip){
         cy.get(this.zipele).clear().type(zip)
@@ -38,9 +40,9 @@ class PharmacyPage{
     clickZiphistoryarrow(){
         cy.get(this.Ziphistoryarrow).click()
     }
-    enterAddress(address){
-        cy.get(this.addressEle,{ timeout: 10000 }).type(address)
-    }
+    //enterAddress(address){
+     //   cy.get(this.addressEle,{ timeout: 10000 }).type(address)
+    //}
     clickFindPahramcyinzipcode(){
         cy.get(this.zipfindphramacyelement).click()
     }
@@ -71,6 +73,15 @@ class PharmacyPage{
     selectSecondPharmacy(){
         cy.get(this.secondpharmacyCheckEle).click()
     }
+    selectThirdPharmacy(){
+        cy.get(this.thirdpharmacyCheckEle).click()
+    }
+    selectFourthPharmacy(){
+        cy.get(this.fourthpharmacyCheckele).click()
+    }
+    selectFifthpharmacy(){
+        cy.get(this.fifthpharmacycheckEle).click()
+    }
     clickGoogleBtn(){
         cy.get(this.googlemapRadioBtnEle).click()
     }
@@ -80,12 +91,6 @@ class PharmacyPage{
     enterZipcodePharmacy(Pname){
         cy.get(this.Zipcodepharamcyele).clear().type(Pname)
     }
-    //clickPharmacyagain(){
-     //   cy.get(this.pharmacycheckEle).click()
-    //}
-    //checkZipRadio(){
-     //   cy.get(this.zipcoderadiocheckele).check({force:true})
-    //}
     clickNextBtn(){
         cy.get(this.nextbtnEle).click({force:true})
     }
@@ -94,15 +99,6 @@ class PharmacyPage{
     }
     selectZiphistoryZipcode(){
         cy.get(this.selecttheziphistoryele).click()
-    }
-    pickPharmacyfromAddress(){
-        cy.get(this.pickpharmacyfromadddressEle).click()
-    }
-    choosePharmacyaddress(){
-        cy.get(this.choosepharmacyaddresele).click()
-    }
-    checkPharmacyagaininaddress(){
-        cy.get(this.checkpharmacyagainfromaddressele).click()
     }
 
 }
